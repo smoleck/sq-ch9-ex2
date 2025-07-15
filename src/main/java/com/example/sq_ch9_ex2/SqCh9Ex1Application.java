dc19930500c5894d3fb655eaf52cca88ce0ad5f4
smoleck <kacper.smolkowski@gmail.com>
2025-07-16 00:05:43 +0200

    session

--- src/main/java/com/example/sq_ch9_ex2/SqCh9Ex1Application.java
+++ src/main/java/com/example/sq_ch9_ex2/SqCh9Ex1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqCh9Ex2Application {
+public class SqCh9Ex1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqCh9Ex2Application.class, args);
+		SpringApplication.run(SqCh9Ex1Application.class, args);
 	}
 
 }
