e2c10774a230b12820c8871c8a6810e8e36688a7
smoleck <kacper.smolkowski@gmail.com>
2025-07-15 23:39:14 +0200

    init

---
+++ src/main/java/com/example/sq_ch9_ex2/SqCh9Ex2Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch9_ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh9Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh9Ex2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sq_ch9_ex2/SqCh9Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch9_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh9Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
